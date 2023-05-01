5e86cbf3cb02c775cfa13e60331b630dc8b4f053
Stephen Marshall <stephenmarshall@Stephens-MacBook-Pro.local>
2023-05-01 14:39:26 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [records, setRecords] = useState([]);
+
+  const baseId = 'appBd6NK5BJ3l7YMC';
+  const apiKey = 'keyeGJgv1WpWyYP6Y';
+  const tableId = 'tbl8z9fwJIHBaWOTf';
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+
+  useEffect(() => {
+    const fetchRecords = async () => {
+      try {
+        const response = await fetch(`https://api.airtable.com/v0/${baseId}/${tableId}?view=viwdjvRLSeQY3vw15`, {
+          headers: {
+            'Authorization': `Bearer ${apiKey}`
+          }
+        });
+        const data = await response.json();
+        setRecords(data.records);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (urlParams.has('Genre')) {
+      fetchParameters('Genre');
+    } else if (urlParams.has('Author')) {
+      fetchParameters('Author');
+    } else if (urlParams.has('Region')) {
+      fetchParameters('Region');
+    } else {
+      fetchRecords();
+    }
+  }, []);
+
+  const fetchParameters = async (param) => {
+    try {
+      const tag = urlParams.get(param);
+      const response = await fetch(`https://api.airtable.com/v0/${baseId}/${tableId}?view=viwdjvRLSeQY3vw15&filterByFormula=${param}='${tag}'`, {
+        headers: {
+          'Authorization': `Bearer ${apiKey}`
+        }
+      });
+      const data = await response.json();
+      setRecords(data.records);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1><a href="index.html">caaarto</a></h1>
+      </div>
+
+      <div className="wrapper">
+        <div id="grid" className="grid">
+          {records.map(record => (
+            <div key={record.id} className="grid-item">
+              <a href={record.fields.Link} target="_blank" className="map-link">
+                <img src={record.fields.Attachments[0].url} onLoad={(e) => e.target.style.height = 'auto'} />
+              </a>
+              {record.fields.Author && (
+                <a href={`?Author=${record.fields.Author}`} className="tag">{record.fields.Author}</a>
+              )}
+              {record.fields.Genre && (
+                <a href={`?Genre=${record.fields.Genre}`} className="tag">{record.fields.Genre}</a>
+              )}
+              {record.fields.Region && (
+                <a href={`?Region=${record.fields.Region}`} className="tag">{record.fields.Region}</a>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
